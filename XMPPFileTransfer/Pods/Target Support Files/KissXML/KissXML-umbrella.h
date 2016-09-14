8a17ca2e726664d68e57cb8ef796072ac271fede
manish Kumar <manishkumar@manishs-MacBook-Pro.local>
2016-09-14 16:59:59 +0530

    added XMPP framwork with CocoaPod

---
+++ XMPPFileTransfer/Pods/Target Support Files/KissXML/KissXML-umbrella.h
@@ -0,0 +1,20 @@
+#import <UIKit/UIKit.h>
+
+#import "DDXMLElementAdditions.h"
+#import "NSString+DDXML.h"
+#import "DDXML.h"
+#import "DDXMLDocument.h"
+#import "DDXMLElement.h"
+#import "DDXMLNode.h"
+#import "KissXML.h"
+#import "DDXMLElementAdditions.h"
+#import "NSString+DDXML.h"
+#import "DDXML.h"
+#import "DDXMLDocument.h"
+#import "DDXMLElement.h"
+#import "DDXMLNode.h"
+#import "KissXML.h"
+
+FOUNDATION_EXPORT double KissXMLVersionNumber;
+FOUNDATION_EXPORT const unsigned char KissXMLVersionString[];
+
